e8cc98e0ed3743c4993dd1730fa216d54d18444c
Bill <njorogekamau63@gmail.com>
2022-08-31 04:19:21 -0700

    models

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""__init__ magic method for models directory"""
+"""
+creating a unique FileStorage instance
+for the application.
+"""
 from models.engine.file_storage import FileStorage
-
 
 storage = FileStorage()
 storage.reload()
